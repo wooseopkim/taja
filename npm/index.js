d8271881d99352da03bbcb1488dfb6a2b6c0d6cd
linterpreteur <guanadah@gmail.com>
2017-02-11 23:47:29 +0900

    Enable taja.is functions

--- npm/index.js
+++ npm/index.js
@@ -1,1 +1,19 @@
-(typeof window == 'undefined' ? {} : window).taja = (typeof module == 'undefined' ? {} : module).exports = require('./taja-fastopt').taja()
+var out = typeof window == 'undefined'
+    ? {obj: module, key: 'exports'}
+    : {obj: window, key: 'taja'}
+var taja = (function (taja) {
+  taja.is = {}
+  var keys = []
+  for (var key in taja) keys.push(key)
+  var i = 0, len = keys.length;
+  for (; i < len; ++i) {
+    var key = keys[i]
+    var matches = key.match(/is([A-Z][a-zA-Z]*)/)
+    if (!matches) continue
+    var fn = taja[key]
+    var kind = matches[1][0].toLowerCase() + matches[1].substring(1)
+    taja.is[kind] = fn.bind(taja)
+  }
+  return taja
+})(require('./taja-fastopt').taja())
+out.obj[out.key] = taja
